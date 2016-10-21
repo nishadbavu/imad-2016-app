8bdb13c2edfcee20cdaecebce70d4c1827bc5577
nishadbavu <nishadkrkara@gmail.com>
2016-10-21 15:15:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-    res.send("Article one to be constructed");
+    res.sendFile(path.join(__dirname,'ui',article-one.html));
 });
 app.get('/Article-two',function(req,res){
     res.send("Article to be constructred");
