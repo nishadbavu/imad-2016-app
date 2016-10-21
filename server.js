c4840b7bd01331a9bb31c6ac822bb90f19ec9dec
nishadbavu <nishadkrkara@gmail.com>
2016-10-21 10:38:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res){
-    res.send("Article to be constructed");
-});
-app.get('/article-three',function(req,res){
-    res.send('the article 3 has been construced');
-});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
