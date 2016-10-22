3c9d4a0050e33997f1ea05dab0878afc6ac37d73
nishadbavu <nishadkrkara@gmail.com>
2016-10-22 11:00:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get(':/articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     articleName=req.params.articleName;
     res.send(create(articles[articleName]));
 });
