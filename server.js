a4769404398473a9f2ecd25b3bb879ee5b70f445
nishadbavu <nishadkrkara@gmail.com>
2016-10-22 07:02:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,42 @@ var articleone=
             
      
 };
+function createTemplate(data)
+{
+    var title=data.title;
+    var hedding=data.hedding;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=
+`
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width initial-scale=1">
+        <title>${title} </title>
+        <link rel="stylesheet" type="text/css" href="ui/style.css">
+    </head>
+    <body>
+        <div class="cont">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h1 class="new">${hedding}</h1>
+        <div>
+            <div>
+                ${date}
+            </div>
+           
+        </div>
+        <div>
+        ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return(htmlTemplate);
+}
 
 
 app.get('/', function (req, res) {
@@ -32,7 +68,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.send(createTemplate(artilceone));
 });
 app.get('/Article-two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
